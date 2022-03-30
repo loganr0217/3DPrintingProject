e9427231cc3a362d23628808e87220b1357e2f4f
Alexander Nemecek <an440818@ohio.edu>
2022-03-29 20:10:53 -0400

    step 2A. highlighting window buttons is the placeholder for user clicks

--- svg-design-demo/src/app/components/stage2/stage2.component.ts
+++ svg-design-demo/src/app/components/stage2/stage2.component.ts
@@ -12,4 +12,34 @@ export class Stage2Component implements OnInit {
   ngOnInit(): void {
   }
 
+  color_change_status = 0;
+
+  click_button1(): void {
+    this.color_change_status = 1;
+  }
+  click_button2(): void {
+    this.color_change_status = 2;
+  }
+  click_button3(): void {
+    this.color_change_status = 3;
+  }
+  click_button4(): void {
+    this.color_change_status = 4;
+  }
+  click_button5(): void {
+    this.color_change_status = 5;
+  }
+  click_button6(): void {
+    this.color_change_status = 6;
+  }
+  click_button7(): void {
+    this.color_change_status = 7;
+  }
+  click_button8(): void {
+    this.color_change_status = 8;
+  }
+  click_button9(): void {
+    this.color_change_status = 9;
+  }
+
 }
