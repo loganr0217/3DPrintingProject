8200f7f9cb8efd81b300f01cbd33b4f1035313fd
lr626019 <lr626019@ohio.edu>
2022-03-30 02:00:54 -0400

    Added step 2 images and breadcrumbs

--- svg-design-demo/src/app/components/stage2/stage2.component.ts
+++ svg-design-demo/src/app/components/stage2/stage2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedDataService } from 'src/app/services/shared-data.service';
 
 @Component({
   selector: 'app-stage2',
@@ -7,39 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Stage2Component implements OnInit {
 
-  constructor() { }
+  constructor(private sharedDataService:SharedDataService) { }
+  
+  // Stage 2 attributes
+  dividerType:string;
+  windowShape:string;
+  windowShapes:string[];
 
   ngOnInit(): void {
+    this.dividerType = this.sharedDataService.selectedDividerType;
+    this.windowShape = this.sharedDataService.selectedWindowShape;
+    this.windowShapes = this.sharedDataService.windowShapes;
   }
 
-  color_change_status = 0;
-
-  click_button1(): void {
-    this.color_change_status = 1;
-  }
-  click_button2(): void {
-    this.color_change_status = 2;
-  }
-  click_button3(): void {
-    this.color_change_status = 3;
-  }
-  click_button4(): void {
-    this.color_change_status = 4;
-  }
-  click_button5(): void {
-    this.color_change_status = 5;
-  }
-  click_button6(): void {
-    this.color_change_status = 6;
-  }
-  click_button7(): void {
-    this.color_change_status = 7;
-  }
-  click_button8(): void {
-    this.color_change_status = 8;
-  }
-  click_button9(): void {
-    this.color_change_status = 9;
+  displayWindowShapes(dividerType:string) {
+    // Adding new window shapes
+    for(let i = 0; i < this.windowShapes.length; ++i) {
+      document.getElementById("windowShapeImage_"+this.windowShapes[i])?.setAttribute("src", "/assets/img/windowButtons/"+this.windowShapes[i]+this.dividerType+".svg");
+    }
   }
 
+  // Method to change the currently selected divider
+  chooseDivider(dividerType:string) {
+    // Unhighlighting old selection if possible and highlighting new one
+    if(this.dividerType != null) {document.getElementById("dividerTypeImage_"+this.dividerType)?.setAttribute("style", "");}
+    document.getElementById("dividerTypeImage_"+dividerType)?.setAttribute("style", "filter: invert(8%) sepia(100%) saturate(7101%) hue-rotate(248deg) brightness(104%) contrast(144%);");
+
+    // Updating values for dividerType
+    this.sharedDataService.selectedDividerType = dividerType;
+    this.dividerType = dividerType;
+
+    this.displayWindowShapes(dividerType);
+  }
+
+  // Method to change the currently selected window shape
+  chooseWindowShape(windowShape:string) {
+    // Unhighlighting old selection if possible and highlighting new one
+    if(this.windowShape != null) {document.getElementById("windowShapeImage_"+this.windowShape)?.setAttribute("style", "");}
+    document.getElementById("windowShapeImage_"+windowShape)?.setAttribute("style", "filter: invert(58%) sepia(100%) saturate(7101%) hue-rotate(200deg) brightness(104%) contrast(144%);");
+
+    // Updating values for windowShape
+    this.sharedDataService.selectedWindowShape = windowShape;
+    this.windowShape = windowShape;
+  }
 }
