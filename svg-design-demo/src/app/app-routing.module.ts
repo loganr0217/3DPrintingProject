743004bc3585fa25e56c3216b85874c150a9b5b4
loganr0217 <44143754+loganr0217@users.noreply.github.com>
2022-04-25 01:39:50 -0400

    Revert "add login signup"

--- svg-design-demo/src/app/app-routing.module.ts
+++ svg-design-demo/src/app/app-routing.module.ts
@@ -1,41 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ForgotPassComponent } from './components/forgot-pass/forgot-pass.component';
-import { FormLoginComponent } from './components/form-login/form-login.component';
-import { FormRegisterComponent } from './components/form-register/form-register.component';
-import { MainPageComponent } from './components/main-page/main-page.component';
-import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { YourIdentifyComponent } from './components/verify-identity/your-identify/your-identify.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'light-screen', pathMatch: 'full' },
-  { path: 'light-screen', component: MainPageComponent
-    // children: [
-    //   { path: '/', redirectTo: 'detail', terminal: true },
-    //   { path: 'detail', component: HeroDetailComponent }
-    // ] 
-  },
-  {
-    path: "login",
-    component: FormLoginComponent
-  },
-  {
-    path: "signup",
-    component: FormRegisterComponent
-  },
-  {
-    path: "verify-code",
-    component: YourIdentifyComponent
-  },
-  {
-    path: "reset-pass",
-    component: ResetPasswordComponent
-  },
-  {
-    path: "forgot-pass",
-    component: ForgotPassComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
