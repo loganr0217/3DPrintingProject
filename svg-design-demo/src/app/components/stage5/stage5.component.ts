bd3b8541179d444131d868499454a1f9ca2aec1c
loganr0217 <lrichards0217@gmail.com>
2022-07-18 01:53:04 -0400

    Adding order saving for signed in users

--- svg-design-demo/src/app/components/stage5/stage5.component.ts
+++ svg-design-demo/src/app/components/stage5/stage5.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { SharedDataService } from 'src/app/services/shared-data.service';
 @Component({
@@ -7,7 +8,7 @@ import { SharedDataService } from 'src/app/services/shared-data.service';
 })
 export class Stage5Component implements OnInit {
 
-  constructor(private sharedDataService:SharedDataService) { }
+  constructor(private sharedDataService:SharedDataService, private http:HttpClient) { }
   getPanelWidth(top:boolean = true):string {
     let width:number = top ? this.sharedDataService.windowWidth : this.sharedDataService.bottomSashWidth;
     let vertDividers:number = this.sharedDataService.dividerNumbers[1] ? this.sharedDataService.dividerNumbers[1] : 0;
@@ -115,10 +116,6 @@ export class Stage5Component implements OnInit {
   ngOnInit(): void {
   }
 
-  saveDesign():void {
-    //
-  }
-
   goToStage4():void {
     document.getElementById("stage4")?.setAttribute("style", "visibility:visible;")
     document.getElementById("stage4")?.scrollIntoView({behavior: 'smooth'});
@@ -132,6 +129,42 @@ export class Stage5Component implements OnInit {
   isDoubleHung():boolean {
     if(this.sharedDataService.selectedWindowShape.substring(0, 2) == "2x") {return true;}
     return false;
+  }
+
+  saveDesign():void {
+    // Setting up vars to get final info for order
+    const email:any = this.sharedDataService.userInfo.length > 1 ? this.sharedDataService.userInfo[3] : null;
+    const password:string = this.sharedDataService.userInfo.length > 1 ? this.sharedDataService.userInfo[4] : "";
+    const selectedDividerType:string = this.sharedDataService.selectedDividerType;
+    const unitChoice:string = this.sharedDataService.unitChoice;
+    const windowWidth:number = this.convertBackNumber(this.sharedDataService.windowWidth, this.sharedDataService.unitChoice);
+    const windowHeight:number = this.convertBackNumber(this.sharedDataService.windowHeight, this.sharedDataService.unitChoice);
+    const horzDividers:number = this.sharedDataService.dividerNumbers[0];
+    const vertDividers:number = this.sharedDataService.dividerNumbers[1];
+    const dividerWidth:number = this.convertBackNumber(this.sharedDataService.dividerWidth, this.sharedDataService.unitChoice);
+    const templateID:number = this.sharedDataService.selectedTemplateID;
+    let panelColoringString:string = "";
+    for(let i:number = 0; i < this.sharedDataService.panelColoringArray.length; ++i) {
+      panelColoringString += this.sharedDataService.panelColoringArray[i].join(",");
+      if(i != this.sharedDataService.panelColoringArray.length - 1) {panelColoringString += ";";}
+    }
+    // console.log(panelColoringString);
+    const streetAddress:string = (<HTMLInputElement>document.getElementById("streetAddressInput")).value;
+    const city:string = (<HTMLInputElement>document.getElementById("cityInput")).value;
+    const state:string = (<HTMLInputElement>document.getElementById("stateInput")).value;
+    const zipcode:string = (<HTMLInputElement>document.getElementById("zipcodeInput")).value;
+    const country:string = "US";
+    const bottomWindowWidth:number = this.isDoubleHung() ? this.convertBackNumber(this.sharedDataService.bottomSashWidth, this.sharedDataService.unitChoice) : 0;
+    const bottomWindowHeight:number = this.isDoubleHung() ? this.convertBackNumber(this.sharedDataService.bottomSashHeight, this.sharedDataService.unitChoice) : 0;
+    // console.log(streetAddress + " " + city + " " + state);
+    this.http.get("https://backend-dot-lightscreendotart.uk.r.appspot.com/saveorder?email='"+email
+    +"'&password='"+password+"'&selectedDividerType='"+selectedDividerType+"'&unitChoice='"+unitChoice
+    +"'&windowWidth="+windowWidth+"&windowHeight="+windowHeight+"&horzDividers="+horzDividers
+    +"&vertDividers="+vertDividers+"&dividerWidth="+dividerWidth
+    +"&templateID="+templateID+"&panelColoringString='"+panelColoringString
+    +"'&streetAddress='"+streetAddress+"'&city='"+city+"'&state='"+state
+    +"'&zipcode='"+zipcode+"'&country='"+country+"'&bottomWindowWidth="+bottomWindowWidth+
+    "&bottomWindowHeight="+bottomWindowHeight).subscribe(result => alert("Success! Your order has been placed."));
   }
 
 }
