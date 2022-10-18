84124935e1ffc530b889765f86c2e89e84812185
loganr0217 <44143754+loganr0217@users.noreply.github.com>
2022-10-18 14:18:42 -0400

    Delete svg-design-demo directory

--- svg-design-demo/src/app/models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    id:number;
-    firstName:string;
-    lastName:string;
-    userEmail:string;
-    userPassword:string;
-    lightscreenOrders:string;
-    permissions:string;
-}
