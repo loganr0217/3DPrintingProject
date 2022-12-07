76a853c42871fc8978b12deed6990e4505e674f8
loganr0217 <lrichards0217@gmail.com>
2022-12-06 22:15:52 -0500

    File method to bypass windows command character limit

--- svgToSTLScript/svgFileGenerator.py
+++ svgToSTLScript/svgFileGenerator.py
@@ -1,7 +1,12 @@
 import sys
+import os
 
-# Argument format : [svgWidth, svgHeight, fileText, fileName]
+# Argument format : [svgWidth, svgHeight, fileText, fileName] --> fileText is now in dummyFile.txt
 arguments = sys.argv
 
-with open(arguments[4]+".svg", "w") as f:
-    f.write(arguments[3])
+with open(arguments[3]+".txt") as f:
+    lines = f.read()
+os.remove(arguments[3]+".txt")
+
+with open(arguments[3]+".svg", "w") as f:
+    f.write(lines)
